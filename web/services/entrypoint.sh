7ee1a00d5dc112195e44e5ea54b3cceb50b3994f
xiaozhi5448 <silenceaxx@gmail.com>
2020-10-13 04:44:25 -0400

    add volumes and network

--- web/services/entrypoint.sh
+++ web/services/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# waiting for db service
+
+sleep 8
+
 # creating database
 echo "creating database"
 
 # Apply database migrations
 echo "Apply database migrations"
+python manage.py makemigrations
 python manage.py migrate
 
 # Start server
